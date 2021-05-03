399a4e190cb788412a50b8b51309905c4e5372c6
Christopher Villaran <villaran.christopher@gmail.com>
2021-05-03 20:07:47 +0800

    removed code for testing

--- src/pages/exercise-2/Exercise2.js
+++ src/pages/exercise-2/Exercise2.js
@@ -11,16 +11,9 @@ import { Accordion } from '@components';
 const Exercise2 = () => {
 
   // console.log(getViewportWidth());
-  const xyz = JSON.parse(JSON.stringify(data)).concat(
-    JSON.parse(JSON.stringify(data)),
-    JSON.parse(JSON.stringify(data)),
-    JSON.parse(JSON.stringify(data)),
-    JSON.parse(JSON.stringify(data)),
-    JSON.parse(JSON.stringify(data))
-  );
 
   const propsToPass = {
-    data: xyz
+    data
   };
 
   return (
